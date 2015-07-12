806092ef173a6b599d69b6b221e4b0e87b68c0a7
siwaweswongcharoen <siwaoh@gmail.com>
2015-07-12 17:14:19 +0700

    refactor

--- src/checkit_test/API.java
+++ src/checkit_test/API.java
@@ -6,6 +6,7 @@ public class API {
 	public static final String CLOUDANT_ACCOUNT = "9196b774-7d5e-41c0-b503-b246651a00f0-bluemix";
 	public static final String CLOUDANT_KEY = "ainyuendetirsedstingstre";
 	public static final String CLOUDANT_PASSWORD = "oVvTL7xGhVLG0EQHy4ajyjrO";
+	public static final String CLOUDANT_DATABASE_NAME = "test";
 	
 	
 }
